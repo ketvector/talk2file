37f1b812cde87a4f683bfc1d660a6f2e5d5fcb34
Neeraj Kumar <kumar7neeraj.yadav@gmail.com>
2024-07-25 12:17:25 +0530

    file upload

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 from dotenv import load_dotenv
 
-from .routes import oaiasst, langchain
+from .routes import oaiasst, langchain, file
 
 load_dotenv(override=True)
 
 app = FastAPI()
 app.include_router(oaiasst.router)
 app.include_router(langchain.router)
+app.include_router(file.router)
 
 @app.get("/")
 async def root():
---
+++ src/routes/file.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+import aiofiles
+
+from fastapi import APIRouter, File, UploadFile
+import os
+
+router = APIRouter(
+    prefix="/file",
+    tags=["file"]
+)
+
+@router.post("/upload/")
+async def create_upload_file(file: UploadFile, id: str):
+    outpath_file = f"{os.environ['LOCAL_FILE_UPLOAD_DIRECTORY']}/{id}.pdf"
+    async with aiofiles.open(outpath_file, 'wb+') as out_file:
+        content = await file.read()  # async read
+        await out_file.write(content)  # async write
+
+    return {"in_filename": file.filename , "id": id, "result": "OK"}
